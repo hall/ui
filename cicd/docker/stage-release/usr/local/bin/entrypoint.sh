c1d39eb5fab18c72a9fe12e3fc495aecc28149f6
Bryton Hall <email@bryton.io>
2020-04-03 16:47:05 -0400

    fix environment variable check in entrypoint

--- cicd/docker/stage-release/usr/local/bin/entrypoint.sh
+++ cicd/docker/stage-release/usr/local/bin/entrypoint.sh
@@ -8,10 +8,13 @@ else
 fi
 
 envsub() {
-    eval val="\$$(set | grep "^$1.*=" | cut -f 1 -d '=')"
+    var=$(set | grep "^$1=")
+    if [ "${var}" != "" ]; then
+        eval val="\$$(echo $var | cut -f1 -d=)"
 
-    sed -i'' "s|#####${1}#####|${val}|g;" /usr/share/nginx/html/main*.js*
-    echo "Configured with ${1}=${val}"
+        sed -i'' "s|#####${1}#####|${val}|g;" /usr/share/nginx/html/main*.js*
+        echo "Configured with ${1}=${val}"
+    fi
 }
 
 envsub HF_SERVER
